fe01fbe9cdd38025c24bb7450ed8484ffe9684b0
超山春卷 <2998772276@qq.com>
2022-11-07 21:12:31 +0800

    2022 11 08

---
+++ practice c github/practice c github/practice c github.cpp
@@ -0,0 +1,48 @@
+//#include <stdio.h>
+//#pragma warning (disable:4996)
+////int pow(int x, int y)
+////{
+////	int n = 1;
+////	for (int k = 0; k < y; k++)
+////	{
+////		
+////		n = n * x;
+////		
+////	}
+////	return n;
+////}
+////int main()
+////{
+////	int a = 2;
+////	int b = 5;
+////	int n = pow(a, b);
+////	printf("%d", n);
+////
+////	return 0;
+//int main()
+//{
+//	printf("*********************************\n");
+//	printf("********  1.    play     ********\n");
+//	printf("********  2.    exit     ********\n");
+//	printf("*********************************\n");
+//	select:
+//	printf("请选择  ");
+//	int a = 0;
+//	scanf("%d", &a);
+//	if (a == 1)
+//	{
+//		printf("即将开始\n");
+//
+//	}
+//	else if (a == 2)
+//	{
+//		printf("即将退出\n");
+//	}
+//	else
+//	{
+//		printf("请重新选择\n");
+//		goto select;
+//	}
+//
+//	return 0;
+//}
